61ddbcdfad29c6a08fec2c98a7b7d77da603d27b
johnlindsay93 <lindsayjohn1993@gmail.com>
2020-11-25 17:42:59 +0000

    deployment test

--- application.py
+++ application.py
@@ -21,15 +21,8 @@ def home():
 
 @app.route('/result', methods=['GET', 'POST'])
 def result():
-    form = BloodPressureForm()
-    calculations = Calculations()
-    if form.validate_on_submit():
-        flash(f'Info Submitted for calculation for {form.name.data}!', 'Success')
-        systolic_level = int(request.form.get("systolic_level"))
-        diastolic_level = int(request.form.get("diastolic_level"))
-        result = calculations.calculate_blood_pressure(systolic_level, diastolic_level)
-        return render_template('result.html', value=result, form=form)
-    return render_template('home.html', form=form)
+
+    return render_template('result.html')
 
 
 if __name__=='__main__':
