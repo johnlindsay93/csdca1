830aeee17e03306ef27ff2614b843a721a7d12e7
johnlindsay93 <lindsayjohn1993@gmail.com>
2020-11-25 17:36:19 +0000

    Fixing unit tests

--- calculations.py
+++ calculations.py
@@ -3,6 +3,7 @@
 class Calculations:
 
     def calculate_blood_pressure(self, systolic_level, diastolic_level):
+
         if systolic_level < 90 and diastolic_level < 60:
             bloodpressure = "Low"
         elif 90 <= systolic_level < 120:
@@ -11,9 +12,9 @@ class Calculations:
             elif 80 < diastolic_level < 90:
                 bloodpressure = "Pre-High"
         elif 120 <= systolic_level < 140:
-            if 80 <= diastolic_level < 90:
+            if 60 <= diastolic_level < 90:
                 bloodpressure = "Pre-High"
-            elif diastolic_level >90:
+            elif diastolic_level > 90:
                 bloodpressure = "High"
         elif systolic_level > 140 or diastolic_level > 90:
             bloodpressure = "High"
