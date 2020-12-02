649848211054a777c21297de9145ccc4e85ee22c
johnlindsay93 <lindsayjohn1993@gmail.com>
2020-12-02 20:45:04 +0000

    adding k6

---
+++ script.js
@@ -0,0 +1,17 @@
+import http from 'k6/http';
+import { check, group, sleep } from 'k6';
+export let options = {
+  stages: [
+    { duration: '30s', target: 10 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
+    { duration: '90s', target: 20 }, // stay at 100 users for 10 minutes
+    { duration: '30s', target: 0 }, // ramp-down to 0 users
+  ],
+  thresholds: {
+    http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+  },
+};
+
+export default function(){
+    http.get('http://johnswebapp-qa.azurewebsites.net/');
+    sleep(1);
+}
