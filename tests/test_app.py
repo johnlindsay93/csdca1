a021f888de4db805f57bab02c46aeac6e6030b53
johnlindsay93 <lindsayjohn1993@gmail.com>
2020-12-13 22:29:57 +0000

    Fixing unit tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,7 @@
 def test_success_load(app, client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Welcome to Blood Pressure App - Version 2" in response.data
+    assert b"Welcome to Blood Pressure App - Version 4" in response.data
     assert b"Username" in response.data
     assert b"systolic_level" in response.data
 
